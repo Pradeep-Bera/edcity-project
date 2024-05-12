3d7c3e61f4e80413f22c853ee30ed81e293847ff
Pradeep-bera <pradeepbera91@gmail.com>
2024-05-13 00:30:00 +0530

    new component ad

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,19 @@
 // import React from 'react'
+import Hero from './Components/Hero/Hero'
 import Navbar from './Components/Navbar/Navbar'
+import Programs from './Components/Programs/Programs';
 
 const App = () => {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Hero />
+      <div className='container'>
+        <Programs />
+      </div>
+    </>
+
   )
 }
 
-export default App
+export default App;
