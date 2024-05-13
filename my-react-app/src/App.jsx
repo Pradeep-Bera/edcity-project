7e85bca32a81b73370deaafa615c339cbdf4b6c2
Pradeep-bera <pradeepbera91@gmail.com>
2024-05-13 21:41:08 +0530

    add some update

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,9 @@
 // import React from 'react'
 import Hero from './Components/Hero/Hero'
 import Navbar from './Components/Navbar/Navbar'
-import Programs from './Components/Programs/Programs';
+import About from './Components/About/About'
+import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
 
 const App = () => {
   return (
@@ -9,7 +11,10 @@ const App = () => {
       <Navbar />
       <Hero />
       <div className='container'>
+        <Title subTitle='Our PROGRAM' title='What We Offer' />
         <Programs />
+        <About />
+
       </div>
     </>
 
---
+++ my-react-app/src/Components/About/About.jsx
@@ -0,0 +1,37 @@
+// import React from 'react'
+import './About.css'
+import about_img from '../../assets/about.png'
+import play_icon from '../../assets/play-icon.png'
+
+const About = () => {
+    return (
+        <div className='about'>
+            <div className='about-left'>
+                <img src={about_img} alt='' className='about-img' />
+                <img src={play_icon} alt='' className='play-icon' />
+            </div>
+            <div className='about-right'>
+                <h3>ABOUT UNIVERSITY</h3>
+                <h2>Nurturing Tomorrows Leaders Today</h2>
+                <p>Emabark on a transformative educational journet with our universitys comprehensive education programs. our cutting -edge
+                    Emabark on a transformative educational journet with our universitysEmabark on a transformative educational journet with our universitys
+
+                </p>
+                <p>Emabark on a transformative educational journet with our universitys comprehensive education programs. our cutting -edge
+                    Emabark on a transformative educational journet with our universitysEmabark on a transformative educational journet with our universitys
+
+                </p>
+                <p>Emabark on a transformative educational journet with our universitys comprehensive education programs. our cutting -edge
+                    Emabark on a transformative educational journet with our universitysEmabark on a transformative educational journet with our universitys
+
+                </p>
+
+
+            </div>
+
+
+        </div>
+    )
+}
+
+export default About
